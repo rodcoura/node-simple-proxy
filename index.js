2cc61cf34eaf8d6dd2d1b18f3c11fd4a7d3e76af
rodcoura <rrcoura@gmail.com>
2022-05-08 22:49:19 -0300

    3 files modified, 1 added

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const request = require('request');
+const dotenv = require('dotenv');
 const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+dotenv.config();
+
+app.use('/*', (req, res) => {
+  console.log(req);
+  req.pipe(request({ qs: req.query, uri: process.env.PROXY_URL })).pipe(res);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Proxy to ${process.env.PROXY_URL} listening on port ${process.env.PORT}`
+  );
 });
